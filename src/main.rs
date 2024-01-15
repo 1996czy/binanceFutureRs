7f1522f2840b65f9e3ae57dad3c4a50b7f344681
1996czy <53745642+1996czy@users.noreply.github.com>
2024-01-15 13:02:35 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,6 @@ use std::rc::Rc;
 use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
 
-#[derive(Deserialize, Serialize, Debug)]
-struct test {
-    lastUpdateId: i64,
-    E: i64,
-    T: i64,
-}
-
 
 #[tokio::main]
 async fn main() {
@@ -35,76 +28,4 @@ async fn main() {
     tokio::join!(task1, task2);
 
 
-    // let mut stg = Arc::new(Mutex::new(strategy::Strategy::new(rest)));
-    // ls.block_on(&mut rt, async {
-    //     let handle1 = tokio::task::spawn_local(async {
-    //         mkt.start().await
-    //     });
-    //     let handle2 = tokio::task::spawn_local(async {
-    //         act.start().await
-    //     });
-    //     tokio::join!(handle1, handle2);
-    // });
-    // tokio::join!(task1, task2);
-    // set.spawn_local(move || async {task1.await});
-    // set.spawn_blocking(async move || {task2});
-
-    // while let Some(res) = set.join_next().await {
-    //     let idx = res.unwrap();
-    // }
-
-    // ls.run_until(mkt.start()).await;
-    // tokio::join!(task1);
-    // tokio::join!(ls);
-    // drop(ls);
-    // tokio::join!(ls);
-    // rt.spawn_local(task1);
-    // rt.spawn_local(task2);
-
-    // make this event loop runforever
-    let run_forever = async {
-        loop {
-            tokio::time::sleep(tokio::time::Duration::from_millis(100000)).await;
-        }
-    };
-    tokio::join!(run_forever);
 }
-
-// struct Sleeper {
-//     text: i32
-// }
-
-// impl Sleeper {
-//     pub async fn sleep_task(&mut self) {
-//         tokio::time::sleep(tokio::time::Duration::from_millis(10000)).await;
-//         self.text += 1;
-//         println!("{:?}", self.text);
-//     }
-// }
-
-
-// struct CallSleeper<F>
-// where F: Future<Output = ()>
-// {
-//     sleeper: Rc<RefCell<F>>
-// }
-
-
-// impl<F> CallSleeper<F>
-// where F: Future<Output = ()>
-// {
-//     async fn call(&mut self) {
-//         (self.sleeper.borrow_mut()).await;
-//     }
-// }
-
-// #[tokio::main]
-// async fn main() {
-    
-//     // let mut sp = Sleeper { text: 1 };
-//     // let mut s1 = CallSleeper { sleeper: Rc::new(RefCell::new(sp.sleep_task())) };
-//     // let mut s2 = CallSleeper { sleeper: Rc::new(RefCell::new(sp.sleep_task())) };
-//     // let task1 = s1.call();
-//     // let task2 = s2.call();
-//     // tokio::join!(task1, task2);
-// }
