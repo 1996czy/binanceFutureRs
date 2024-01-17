29aa530c7385e26ef4e83c8400d26b4f076a2b81
1996czy <53745642+1996czy@users.noreply.github.com>
2024-01-17 17:31:13 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,26 @@ mod structure;
 use tokio;
 use reqwest;
 use std::rc::Rc;
-use serde::{Deserialize, Serialize};
 use tokio::sync::Mutex;
 
 
 #[tokio::main]
 async fn main() {
-    // let mut rsp = reqwest::get("https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT&limit=500").await.unwrap().json::<test>().await.unwrap();
-    // println!("{:?}", rsp);
     // initialize market and account module
-    let mut rt = tokio::runtime::Builder::new_current_thread().build().unwrap();
-    let mut ls = tokio::task::LocalSet::new();
-    let mut rest = rest::Rest::new();
-    let mut stg = Rc::new(Mutex::new(strategy::Strategy::new(rest)));
-    
-    let streams = "btcusdt@aggTrade";
-    let mut mkt = market::Market::new(streams, stg.clone());
-    let mut act = account::Account::new(stg.clone());
-    let task1 = mkt.start();
-    let task2 = act.start();
-    tokio::join!(task1, task2);
+    let mut stg = Rc::new(Mutex::new(strategy::Strategy::new().await));
+    let mut mkt = market::Market::new();
+    let mut act = account::Account::new();
 
+    // start strategy
+    stg.lock().await.start_handle_rsp().await;
+    let task1 = mkt.start(stg.clone());
+    let task2 = act.start(stg.clone());
 
+    // make this event loop runforever
+    let run_forever = async {
+        loop {
+            tokio::time::sleep(tokio::time::Duration::from_millis(100000)).await;
+        }
+    };
+    tokio::join!(task1, task2, run_forever);
 }
