d596129453ee93b097abc979ba1b716073b60930
1996czy <53745642+1996czy@users.noreply.github.com>
2024-01-17 17:29:55 +0800

    Update rest.rs

--- src/rest.rs
+++ src/rest.rs
@@ -9,30 +9,25 @@ use crate::structure::*;
 type HmacSha256 = Hmac<Sha256>;
 
 
-pub struct Rest<'a> {
-    base_rest: &'a str,
-    pubkey: &'a str,
-    secret: &'a str,
+pub struct Rest {
 }
 
 
-impl<'a> Rest<'a> {
+impl Rest {
     pub fn new() -> Self {
         Rest {
-            base_rest: "https://fapi.binance.com",
-            pubkey: "***",
-            secret: "***"
         }
     }
 
-    fn default_header(&self) -> reqwest::header::HeaderMap {
+    fn default_header() -> reqwest::header::HeaderMap {
+        let pubkey = "*";
         let mut dh = reqwest::header::HeaderMap::new();
         dh.insert("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8;".parse().unwrap());
-        dh.insert("X-MBX-APIKEY", self.pubkey.parse().unwrap());
+        dh.insert("X-MBX-APIKEY", pubkey.parse().unwrap());
         dh
     }
 
-    fn method_client(&self, method: &str, url: &str) -> Option<reqwest::RequestBuilder> {
+    fn method_client(method: &str, url: &str) -> Option<reqwest::RequestBuilder> {
         match method {
             "post" => Some(reqwest::Client::new().post(url)),
             "get" => Some(reqwest::Client::new().get(url)),
@@ -42,24 +37,28 @@ impl<'a> Rest<'a> {
         }
     }
 
-    fn signature(&self, query: &str) -> String {
-        let mut mac = HmacSha256::new_from_slice(self.secret.as_bytes()).expect("");
+    fn signature(query: &str) -> String {
+        // given secret
+        let secret = "*";
+        let mut mac = HmacSha256::new_from_slice(secret.as_bytes()).expect("");
         mac.update(query.as_bytes());
         hex::encode(mac.finalize().into_bytes())
     }
 
-    async fn make_request(&self, header: reqwest::header::HeaderMap, sub_url: &str, body: Option<(&str, &str)>, method: &str) -> Result<reqwest::Response, reqwest::Error> {
+    async fn make_request(header: reqwest::header::HeaderMap, sub_url: &str, body: Option<(&str, &str)>, method: &str) -> Result<reqwest::Response, reqwest::Error> {
+        // give base rest
+        let base_rest = "https://fapi.binance.com";
         // get corret x-www-form url
-        let mut addr = match body {
+        let addr = match body {
             // sign request with body
-            Some((&_, "sign")) => sub_url.to_string() + body.unwrap().0 + "&signature=" + &self.signature(body.unwrap().0),
+            Some((&_, "sign")) => sub_url.to_string() + body.unwrap().0 + "&signature=" + &Self::signature(body.unwrap().0),
             // unsign request with body 
             Some((&_, &_)) => sub_url.to_string() + body.unwrap().0,
             // header only request
             None => sub_url.to_string()
         };
         // generate method client
-        let client = self.method_client(method, &(self.base_rest.to_string() + &addr));
+        let client = Self::method_client(method, &(base_rest.to_string() + &addr));
         // make request
         client.unwrap()
         .headers(header)
@@ -67,13 +66,28 @@ impl<'a> Rest<'a> {
         .await
     }
 
-    pub async fn get_listenKey(&self) -> String{
+    pub async fn get_exchangeInfo() -> ExchangeInfoRsp {
+        // get address
+        let addr = "/fapi/v1/exchangeInfo";
+        // get listenkey req headers
+        let header = Self::default_header();
+        // make request
+        let rsp = Self::make_request(header, addr, None, "get").await;
+        // parse rsp
+        rsp
+        .unwrap()
+        .json::<ExchangeInfoRsp>()
+        .await
+        .unwrap()
+    }
+
+    pub async fn get_listenKey() -> String {
         // listen key address
         let addr = "/fapi/v1/listenKey";
         // get listenkey req headers
-        let header = self.default_header();
+        let header = Self::default_header();
         // make request
-        let rsp = self.make_request(header, addr, None, "post").await;
+        let rsp = Self::make_request(header, addr, None, "post").await;
         // parse rsp
         rsp.expect("")
         .json::<ListenKey>()
@@ -82,73 +96,73 @@ impl<'a> Rest<'a> {
         .listenKey
     }
 
-    pub async fn renew_listenKey(&self, listen_key: &str) {
-        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+    pub async fn renew_listenKey(listen_key: String) {
         // listen key address
         let addr = "/fapi/v1/listenKey";
         // get listenkey req headers
-        let header = self.default_header();
+        let header = Self::default_header();
         // make request
-        let body = &to_string(ListenKey { listenKey: listen_key.to_string() }).unwrap();
-        let rsp = self.make_request(header, addr, Some((body, "")), "put").await;
+        let body = &to_string(ListenKey { listenKey: listen_key }).unwrap();
+        let rsp = Self::make_request(header, addr, Some((body, "")), "put").await;
     }
 
     pub async fn create_order(&self, order: LimitOrder) {
         // order address
         let addr = "/fapi/v1/order";
         // get order req headers
-        let header = self.default_header();
+        let header = Self::default_header();
         // make request
         let body = &to_string(&order).unwrap();
-        let rsp = self.make_request(header, addr, Some((body, "sign")), "post").await;
+        let rsp = Self::make_request(header, addr, Some((body, "sign")), "post").await;
         // parse rsp
         rsp.expect("")
-        .json::<OrderCreateResponse>()
+        .json::<OrderCreateRsp>()
         .await
         .expect("");
     }
 
-    pub async fn amend_order(&self, order: AmendOrder) {
+    pub async fn amend_order(order: AmendOrder) {
         // order address
         let addr = "/fapi/v1/order";
         // get order req headers
-        let header = self.default_header();
+        let header = Self::default_header();
         // make request
         let body = &to_string(&order).unwrap();
-        let rsp = self.make_request(header, addr, Some((body, "sign")), "put").await;
+        let rsp = Self::make_request(header, addr, Some((body, "sign")), "put").await;
         // parse rsp
         rsp.expect("")
-        .json::<OrderAmendResponse>()
+        .json::<OrderAmendRsp>()
         .await
         .expect("");
     }
 
-    pub async fn cancel_order(&self, order: CancelOrder) {
+    pub async fn cancel_order(order: CancelOrder) -> String {
         // order address
         let addr = "/fapi/v1/order";
         // get order req headers
-        let header = self.default_header();
+        let header = Self::default_header();
         // make request
         let body = &to_string(&order).unwrap();
-        let rsp = self.make_request(header, addr, Some((body, "sign")), "delete").await;
+        let rsp = Self::make_request(header, addr, Some((body, "sign")), "delete").await;
         // parse rsp
         rsp.expect("")
-        .json::<OrderCancelResponse>()
+        .json::<OrderCancelRsp>()
         .await
         .expect("");
+        "OK".to_string()
     }
     
-    pub async fn cancel_all_order(&self, order: CancelAllOrder) {
+    pub async fn cancel_all_order(order: CancelAllOrder) {
         // order address
         let addr = "/fapi/v1/allOpenOrders";
         // get order req headers
-        let header = self.default_header();
+        let header = Self::default_header();
         // make request
         let body = &to_string(&order).unwrap();
-        let rsp = self.make_request(header, addr, Some((body, "sign")), "delete").await;
+        let rsp = Self::make_request(header, addr, Some((body, "sign")), "delete").await;
         // parse rsp
         rsp.expect("")
-        .json::<OrderCancelAllResponse>()
+        .json::<OrderCancelAllRsp>()
         .await
         .expect("");
     }
